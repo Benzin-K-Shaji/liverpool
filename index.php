211e6b6ac5c4e3ea1132388c8762ce63d8cdc5b9
Benzin-K-Shaji <mca512@rajagiri.edu>
2021-03-17 01:24:12 +0530

    success

---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trainyathri</title>
+</head>
+<nav class="navbar navbar-expand-lg navbar-light" style="background: #f32409;" > 
+  <div class="container-fluid">
+  <img src="logo.jpg" height="45" alt="" >
+<a class="navbar-brand" href="index.php" style="font-size:larger; font-family:sans-serif; color: cornsilk;">  Home</a>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="aboutus.php" style="font-size:larger; font-family:sans-serif; color: cornsilk;">About Us</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" style="font-size:larger; font-family:sans-serif;"></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="ticketbooking.php" style="font-size:larger; font-family:sans-serif; color: cornsilk;">ticketbooking</a>
+    </li>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="offers.php" style="font-size:larger; font-family:sans-serif; color: cornsilk;">Offers</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="matchsearch.php" style="font-size:larger; font-family:sans-serif; color: cornsilk;">matchsearch</a>
+  </li>
+  
+    
+  </ul>
+  
+</div>
+</div>
+</nav>
+   <marquee style="font-family:Georgia, 'Times New Roman', Times, serif; color:rgb(8, 8, 8) ;" >WELCOME TO ANFIELD||YOU WILL NEVER WALK ALONE<br></marquee>
+<body background="http://www.thehotdesktop.com/web/wallpapers/liverpool-fc/1920x1080.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-8 col-lg-12 col-xl-12 col-xxl-12">
+                <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                      <button type="button" data-bs-target ="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                      <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active" data-bs-interval="10000">
+                        <img src="im78.jpg" height="500" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            
+                            <h5 style="font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; color:rgb(245, 92, 113);"></h5>
+                            <p style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; color:rgb(245, 92, 113);"></p>
+                        </div>
+                      </div>
+                      <div class="carousel-item" data-bs-interval="2000">
+                        <img src="iim.png" height="500" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            
+                            <h5 style="font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; color:floralwhite;"></h5>
+                            <p style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; color:floralwhite;"></p>
+                        </div>
+                      </div>
+                      <div class="carousel-item">
+                        <img src="https://images.pexels.com/photos/1884574/pexels-photo-1884574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" height="500" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5 style="font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; color:floralwhite;"></h5>
+                            <p style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; color: floralwhite;"></p>
+                        </div>
+                      </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"  data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"  data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
+            </div>
+        </div>
+         <h2><br><br><b>P L A Y E R S </b> <br></h2>
+        <div class="row">
+            <div class="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table table-borderless">
+                    <tr>
+                        <td>
+                         <div class="card" style="width: 18rem;">
+                         <img src="https://xtratime.in/wp-content/uploads/2020/11/Mohammed-Salah-1024x1024.jpg" class="card-img-top" alt="...">
+                                 <div class="card-body">
+                                 <h5 class="card-title">Mohamed Salah</h5>
+                                 <p class="card-text">r who plays as a forward for Premier League club Liverpool and captains the Egypt national team.
+
+                                </p>
+                                 <a href="https://www.google.com/search?rlz=1C1CHBD_enIN917IN917&sxsrf=ALeKk010xAKffDwMbsVuG4xdlRZyoNLfFQ%3A1615913131903&ei=q-BQYOvONu3F4-EP_aOlmAY&q=mohamed+salah+career+stats&oq=muhammed+salah+ca&gs_lcp=Cgdnd3Mtd2l6EAEYAzIICAAQFhAKEB4yCAgAEBYQChAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjoHCCMQsAMQJzoHCC4QsAMQQzoHCAAQsAMQQzoHCC4QsAMQCjoHCAAQsAMQCjoECAAQQzoECAAQCjoHCAAQhwIQFDoHCC4QChCTAlCQFFjVJ2DcPWgBcAB4AIAB5QSIAYsOkgEJMi0xLjAuMi4xmAEAoAEBqgEHZ3dzLXdpesgBCsABAQ&sclient=gws-wiz" class="btn btn-primary">stats</a>
+                                 </div>
+                                </div></td>
+                                <td><div class="card" style="width: 18rem;">
+                                 <img src="https://i.guim.co.uk/img/media/5637869d687364255b42fc77ff21157759643d90/0_44_2813_1687/master/2813.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=0f85195cfa946619c396b50695b86dc2"  class="card-img-top" alt="...">
+                                 <div class="card-body">
+                                 <h5 class="card-title">Virgil van Dijk</h5>
+                                 <p class="card-text"> Virgil van Dijk is a Dutch professional footballer who plays as a centre-back for Premier League club Liverpool and captains the Netherlands national team. </p>
+                                 <a href="https://www.google.com/search?sa=X&rlz=1C1CHBD_enIN917IN917&hl=en&sxsrf=ALeKk02IWBMRp_AcZ9wuA-LVrt47DK5_QQ:1615915616889&q=Virgil+van+Dijk&stick=H4sIAAAAAAAAAONgFuLQz9U3MMkpSVPiArGysqsKLLO1hIIL8otKikNSE3ND8oPyi0tSixax8odlFqVn5iiUJeYpuGRmZU9gYwQAw3YuCUEAAAA&ved=2ahUKEwjnodPiqrXvAhXNxDgGHb9XAeYQ-BYwO3oECCMQNg&biw=1536&bih=698&dpr=1.25" class="btn btn-primary">stats</a> 
+                                 </div>
+                                 </div></td>
+                                  <td><div class="card" style="width: 18rem;">
+                                  <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p110979.png" card-img-top" alt="...">
+                                  <div class="card-body">
+                                 <h5 class="card-title">Sadio Mané</h5>
+                                 <p class="card-text">Sadio Mané is a Senegalese professional footballer who plays as a winger for Premier League club Liverpool and the Senegal national team. 
+                                 
+                                 </p>
+                                 <a href=https://www.google.com/search?sa=X&rlz=1C1CHBD_enIN917IN917&hl=en&sxsrf=ALeKk02-s2fymMWagwNpxpgTFE1nQt0TGg:1615915621276&q=Sadio+Man%C3%A9&stick=H4sIAAAAAAAAAONgFuLQz9U3MMkpSVPiArGyjFMMs0u0hIIL8otKikNSE3ND8oPyi0tSixaxcgcnpmTmK_gm5h1eOYGNEQBlaB-CPQAAAA&ved=2ahUKEwjEi9_kqrXvAhXaxjgGHXYlAScQ-BYwPHoECCsQPA&biw=1536&bih=698" class="btn btn-primary">stats</a>
+                                 </div>
+                         </div></td>
+                         </tr>
+                         <tr>
+                          <td>
+                            <div class="card" style="width: 18rem;">
+                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-ZbkjxwhxAjxNSDh1XJMaLnrP_PM5cG1DrYwcRrZZybfbTo7tAFqjkxAq0ebln0ZqxNw&usqp=CAU" class="card-img-top" alt="...">
+                         <div class="card-body">
+                        <h5 class="card-title">Alisson Becker</h5>
+                        <p class="card-text">Alisson Ramses Becker is a Brazilian professional footballer who plays as a goalkeeper for Premier League club Liverpool and the Brazil national team</p>
+                        <a href="https://www.google.com/search?sa=X&rlz=1C1CHBD_enIN917IN917&hl=en&sxsrf=ALeKk02TALWyWguFloWUKQaq_mlArUim0Q:1615915999982&q=Alisson+Becker&stick=H4sIAAAAAAAAAONgFuLQz9U3MMkpSVPiArHy0gssU4q0hIIL8otKikNSE3ND8oPyi0tSixax8jnmZBYX5-cpOKUmZ6cWTWBjBAD4WQt_QAAAAA&ved=2ahUKEwi1qamZrLXvAhWQILcAHd3rCegQ-BYwPnoECC4QSA&biw=1536&bih=698" class="btn btn-primary">stats</a>
+                        </div></td>
+                       </tr>
+                </table>
+             </div>
+     </div>
+
+     <div class="row" style="margin-top: 40px;">
+        <div class="col-md-12 show-on-scroll container_animation">
+          <h2 class="black_txt font-xxl" style="margin-bottom:24px;">Fixtures</h2>
+          <div class="table-responsive train_routes">
+            <table class="table">
+              <thead>
+                 <tr>
+                    <th class="font-xxs fw500">VS</th>
+                    <th class="font-xxs fw500">DATE</th>
+                    <th class="font-xxs fw500">TIME</th>
+  
+                 </tr>
+              </thead>
+              <tbody>
+                  <tr>
+
+                    <td class="black_txt font-xxs">Manchester united</td>
+                    <td class="black_txt font-xxs">12/1/2021
+                    </td>
+                    <td class="black_txt font-xxs">
+                        12:30AM
+                    </td>
+                  </tr>
+                  <tr>
+
+                    <td class="black_txt font-xxs">Leeds united</td>
+                    <td class="black_txt font-xxs">16/1/2021
+                    </td>
+                    <td class="black_txt font-xxs">
+                        12:30AM
+                    </td>
+                  </tr>
+                  <tr>
+
+                    <td class="black_txt font-xxs">Manchester city</td>
+                    <td class="black_txt font-xxs">20/1/2021
+                    </td>
+                    <td class="black_txt font-xxs">
+                        12:30AM
+                    </td>
+                  </tr>
+                  <tr>
+
+                    <td class="black_txt font-xxs">Arsenal</td>
+                    <td class="black_txt font-xxs">25/1/2021
+                    </td>
+                    <td class="black_txt font-xxs">
+                        12:30AM
+                    </td>
+                  </tr>
+                  <tr>
+
+                    <td class="black_txt font-xxs">chelsea</td>
+                    <td class="black_txt font-xxs">30/1/2021
+                    </td>
+                    <td class="black_txt font-xxs">
+                        12:30AM
+                    </td>
+                  </tr>
+                  <tr>
+
+                    <td class="black_txt font-xxs">Aston villa</td>
+                    <td class="black_txt font-xxs">1/2/2021
+                    </td>
+                    <td class="black_txt font-xxs">
+                        12:30AM
+                    </td>
+                  </tr> <tr>
+
+                    <td class="black_txt font-xxs">southampton</td>
+                    <td class="black_txt font-xxs">5/2/2021
+                    </td>
+                    <td class="black_txt font-xxs">
+                        12:30AM
+                    </td>
+                  </tr> <tr>
+
+                    <td class="black_txt font-xxs">Leeds united</td>
+                    <td class="black_txt font-xxs">9/2/2021
+                    </td>
+                    <td class="black_txt font-xxs">
+                        12:30AM
+                    </td>
+                  </tr>
+                  <tr>
+
+                    <td class="black_txt font-xxs">Arsenal</td>
+                    <td class="black_txt font-xxs">12/1/2021
+                    </td>
+                    <td class="black_txt font-xxs">
+                        12:30AM
+                    </td>
+                  </tr>
+                  <tr>
+
+                    <td class="black_txt font-xxs">Chelsea</td>
+                    <td class="black_txt font-xxs">21/2/2021
+                    </td>
+                    <td class="black_txt font-xxs">
+                        12:30AM
+                    </td>
+                  </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+        
+        <div class="row">
+                    <div class="col col-5 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-8">
+                    <img src="Screenshot 2021-03-16 234125.png" width="1200" alt="">   
+                    </div>
+                    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+     </body> 
+</html>
